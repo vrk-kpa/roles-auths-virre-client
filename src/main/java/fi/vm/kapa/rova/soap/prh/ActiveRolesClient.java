37e9ab04ba1a0a4e728b8c1b8e82f29ff286873b
tkarttu <tkarttu@gmail.com>
2016-01-20 12:56:37 +0200

    KAPAROVA-417 muutettu XRoadPersonActiveRoleInfo-soap -responsen parsinta yksivaiheiseksi

--- src/main/java/fi/vm/kapa/rova/soap/prh/ActiveRolesClient.java
+++ src/main/java/fi/vm/kapa/rova/soap/prh/ActiveRolesClient.java
@@ -1,8 +1,5 @@
 package fi.vm.kapa.rova.soap.prh;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.xml.bind.JAXBException;
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Holder;
@@ -10,13 +7,9 @@ import javax.xml.ws.Holder;
 import org.springframework.stereotype.Component;
 
 import fi.vm.kapa.rova.logging.Logger;
-import fi.vm.kapa.rova.soap.prh.model.CompaniesResponseMessage;
-import fi.vm.kapa.rova.soap.prh.model.ExtendedRoleInfo;
-import fi.vm.kapa.rova.soap.prh.model.Role;
 import fi.vrk.xml.rova.prh.activeroles.ObjectFactory;
+import fi.vrk.xml.rova.prh.activeroles.PersonActiveRoleInfoResponseType;
 import fi.vrk.xml.rova.prh.activeroles.PersonActiveRoleInfoType;
-import fi.vrk.xml.rova.prh.activeroles.RoleInCompanyType;
-import fi.vrk.xml.rova.prh.activeroles.RoleInCompanyType.RoleBasicInfo;
 import fi.vrk.xml.rova.prh.activeroles.XRoadPersonActiveRoleInfo;
 import fi.vrk.xml.rova.prh.activeroles.XRoadPersonActiveRoleInfoPortType;
 import fi.vrk.xml.rova.prh.activeroles.XRoadPersonActiveRoleInfoPortTypeService;
@@ -29,7 +22,7 @@ public class ActiveRolesClient extends AbstractPrhClient {
     XRoadPersonActiveRoleInfoPortTypeService service = new XRoadPersonActiveRoleInfoPortTypeService();
     ObjectFactory factory = new ObjectFactory();
 
-    public CompaniesResponseMessage getResponse(String personId) throws JAXBException {
+    public PersonActiveRoleInfoResponseType getResponse(String personId) throws JAXBException {
         XRoadPersonActiveRoleInfoPortType port = service.getXRoadPersonActiveRoleInfoPortTypePort();
         BindingProvider bp = (BindingProvider) port;
         
@@ -42,46 +35,11 @@ public class ActiveRolesClient extends AbstractPrhClient {
         Holder<XRoadPersonActiveRoleInfoResponse> response = new Holder<XRoadPersonActiveRoleInfoResponse>();
         response.value = factory.createXRoadPersonActiveRoleInfoResponse();
 
-        CompaniesResponseMessage result = new CompaniesResponseMessage();
-        try {
-            port.xRoadPersonActiveRoleInfo(request, getClientHeader(factory), getServiceHeader(factory), getUserIdHeader(), getIdHeader(), getProtocolVersionHeader(), response);
-            LOG.info("soap succeeded");
-
-            result.setSocialSec(response.value.getResponse().getSocialSecurityNumber());
-            result.setStatus(response.value.getResponse().getStatus());
-
-
-            List<Role> roles = new ArrayList<>(); // to be set into responseMsg
-
-            for (RoleInCompanyType roleInCompany : response.value.getResponse().getRoleInCompany()) {
-
-                Role role = new Role();
-                role.setBusinessId(roleInCompany.getBusinessId());
-                role.setCompanyName(roleInCompany.getCompanyName());
-
-                List<ExtendedRoleInfo> extendedRoleInfos = new ArrayList<>();
-                
-                for (RoleBasicInfo roleInfo : roleInCompany.getRoleBasicInfo()) {
-                    ExtendedRoleInfo extendedRoleInfo = new ExtendedRoleInfo();
-                    extendedRoleInfo.setRoleType(roleInfo.getName());
-                    extendedRoleInfo.setBodyType(roleInfo.getBodyType());
-                    extendedRoleInfo.setStartDate(roleInfo.getStartDate().toString());
-
-                    extendedRoleInfos.add(extendedRoleInfo);
-                    
-                }
-                role.setExtendedRoleInfos(extendedRoleInfos);
-
-                roles.add(role);
-            }
-            
-            result.setRoles(roles);
-        } catch (Exception e) {
-            LOG.warning("exception: "+ e.getMessage());
-            e.printStackTrace();
-        }
+        port.xRoadPersonActiveRoleInfo(request, getClientHeader(factory), getServiceHeader(factory), 
+                getUserIdHeader(), getIdHeader(), getProtocolVersionHeader(), response);
+        LOG.debug("soap succeeded");
         
-        return result;
+        return response.value.getResponse();
    }
 }
  
--- src/main/java/fi/vm/kapa/rova/virreclient/service/ActiveRolesService.java
+++ src/main/java/fi/vm/kapa/rova/virreclient/service/ActiveRolesService.java
@@ -1,21 +1,24 @@
 package fi.vm.kapa.rova.virreclient.service;
 
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fi.vm.kapa.rova.external.model.virre.Company;
+import fi.vm.kapa.rova.external.model.virre.CompanyPerson;
 import fi.vm.kapa.rova.external.model.virre.CompanyRoleType;
 import fi.vm.kapa.rova.external.model.virre.RoleNameType;
 import fi.vm.kapa.rova.logging.Logger;
 import fi.vm.kapa.rova.soap.prh.ActiveRolesClient;
-import fi.vm.kapa.rova.soap.prh.model.CompaniesResponseMessage;
-import fi.vm.kapa.rova.soap.prh.model.ExtendedRoleInfo;
-import fi.vm.kapa.rova.soap.prh.model.Role;
-import fi.vm.kapa.rova.soap.virre.model.VirreResponseMsg;
+import fi.vrk.xml.rova.prh.activeroles.PersonActiveRoleInfoResponseType;
+import fi.vrk.xml.rova.prh.activeroles.RoleInCompanyType;
+import fi.vrk.xml.rova.prh.activeroles.RoleInCompanyType.RoleBasicInfo;
 
 /**
  * Created by Juha Korkalainen on 15.1.2016.
@@ -28,54 +31,76 @@ public class ActiveRolesService extends ServiceLogging {
     private static final Logger LOG = Logger.getLogger(CompaniesService.class);
 
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ");
-
+    
     @Autowired
     private ActiveRolesClient client;
     
+    @SuppressWarnings("unchecked")
     public List<Company> getCompanies(String hetu) throws VIRREServiceException {
-        List<Company> companies = null;
+        Map<String, List<?>> response = new HashMap<>();
 
         try {
             long startTime = System.currentTimeMillis();
-            CompaniesResponseMessage msg = client.getResponse(hetu);
-            LOG.warning(msg.toString());
+            PersonActiveRoleInfoResponseType result = client.getResponse(hetu);
+            LOG.debug(result.toString());
+            response = parseCompanies(result);
             logRequest(OP + ":RoVaListCompanies", startTime, System.currentTimeMillis());
-            companies = parseCompanies(msg);
         } catch (Exception e) {
             logError(OP, "Failed to parse companies: " + e.getMessage());
             throw new VIRREServiceException(e.getMessage(), e);
         }
 
-        return companies;
+        return (List<Company>) response.get(Company.TYPE);
     }
 
-    private List<Company> parseCompanies(CompaniesResponseMessage msg) {
-        List<Company> companies = new LinkedList<>();
-        if (msg != null) {
-            for (Role role : msg.getRoles()) {
-                Company company = new Company();
-                company.setBusinessId(role.getBusinessId());
-                company.setCompanyName(role.getCompanyName());
-                List<CompanyRoleType> roles = new LinkedList<>();
-                for (ExtendedRoleInfo eri : role.getExtendedRoleInfos()) {
-                    CompanyRoleType cr = new CompanyRoleType();
-                    RoleNameType type = null;
-                    String rt = eri.getRoleType();
-                    try {
-                        type = RoleNameType.valueOf(rt);
-                    } catch (IllegalArgumentException e) {
-                        logWarning(OP, "Unable to parse role: " + rt);
-                    }
-                    cr.setType(type);
-                    roles.add(cr);
-                }
-                company.setRoles(roles);
-                companies.add(company);
-            }
-        } else {
-            logWarning(OP, "Message was null.");
+    private Map<String, List<?>> parseCompanies(PersonActiveRoleInfoResponseType result) {
+        if (result == null) {
+            throw new ServiceException("Parsing failed because of null result (PersonActiveRoleInfoResponseType)");
         }
-        return companies;
+        
+        CompanyPerson person = new CompanyPerson();
+        person.setFirstName(result.getFirstname());
+        person.setLastName(result.getSurname());
+        person.setSocialSec(result.getSocialSecurityNumber());
+        person.setStatus(result.getStatus());
+        
+        List<CompanyPerson> persons = new ArrayList<>(); // list of one person into return map
+        persons.add(person);
+        
+        List<Company> companies = new ArrayList<>(); // list of companies into return map
+        for (RoleInCompanyType roleInCompany : result.getRoleInCompany()) {
+            Company company = new Company();
+            company.setBusinessId(roleInCompany.getBusinessId());
+            company.setCompanyName(roleInCompany.getCompanyName());
+            company.setState(roleInCompany.getState());
+            
+            List<CompanyRoleType> roles = new ArrayList<>();
+            for (RoleBasicInfo roleInfo : roleInCompany.getRoleBasicInfo()) {
+                CompanyRoleType role = new CompanyRoleType();
+                
+                RoleNameType type = null;
+                String roleName = roleInfo.getName();
+                try {
+                    type = RoleNameType.valueOf(roleName);
+                } catch (IllegalArgumentException e) {
+                    logWarning(OP, "Unable to parse role: " + roleName);
+                }
+                role.setType(type);
+                
+                roles.add(role);
+            }
+            company.setRoles(roles);
+            
+            companies.add(company);
+        }
+        
+        Map<String, List<?>> rolesMap = new HashMap<>(); // return value
+        rolesMap.put(CompanyPerson.TYPE, persons);
+        rolesMap.put(Company.TYPE, companies);
+        
+        LOG.debug(rolesMap+"");
+        
+        return rolesMap;
     }
 
 }
