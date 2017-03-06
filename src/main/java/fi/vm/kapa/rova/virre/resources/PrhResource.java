66b057bb5379885a932d297a334cfb51a38a205e
Juha Korkalainen <juha.korkalainen@iki.fi>
2017-03-06 15:42:06 +0200

    clean up

--- src/main/java/fi/vm/kapa/rova/virre/resources/PrhResource.java
+++ src/main/java/fi/vm/kapa/rova/virre/resources/PrhResource.java
@@ -41,7 +41,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
 
-@Configuration
 @RestController
 @RequestMapping("/rest")
 public class PrhResource {
