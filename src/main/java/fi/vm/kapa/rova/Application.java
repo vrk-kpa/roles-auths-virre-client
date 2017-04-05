f647ba5bddb889c07e64e3cd7f054a4ccbe577d4
jteuho <juho.teuho@gofore.com>
2017-04-05 12:25:43 +0300

    eureka-conffeja

--- src/main/java/fi/vm/kapa/rova/Application.java
+++ src/main/java/fi/vm/kapa/rova/Application.java
@@ -25,10 +25,11 @@ package fi.vm.kapa.rova;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
